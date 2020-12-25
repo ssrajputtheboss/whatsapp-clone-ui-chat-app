e32b90975b369e5c49d02fb176a109f5627c005a
ssrajputtheboss <62293640+ssrajputtheboss@users.noreply.github.com>
2020-12-25 18:17:27 +0530

    Update client.js

--- server/client.js
+++ server/client.js
@@ -2,7 +2,7 @@ const Client = require('pg').Client;
 
 const client = new Client({
   user:"postgres",
-  password:"pkmkb98261",
+  password:"yourpassword",
   port: 5432,
   database: "whatsappdata"
 });
